ce80d65bafbce0f351c869dc06d3acb1e0671298
Dawid-P <pukszta.polar@gmail.com>
2021-09-18 20:28:44 +0200

    some css

--- components/Card.js
+++ components/Card.js
@@ -13,12 +13,10 @@ const Card = ({ data, name }) => {
   ratingPerGame = (ratingGain / games.length).toFixed(2);
 
   return (
-    <div className={styles.card}>
+    <div className={ratingGain > 0 ? styles.positive : styles.negative}>
       <p>Name: {name}</p>
-      <p>Number of games: {games.length}</p>
-      <p className={ratingGain > 0 ? styles.positive : styles.negative}>
-        Rating gain: {ratingGain}
-      </p>
+      <p>Games: {games.length}</p>
+      <p>Rating gain: {ratingGain}</p>
       <p>Rating per game: {ratingPerGame}</p>
     </div>
   );
--- components/DataDisplay.js
+++ components/DataDisplay.js
@@ -39,10 +39,6 @@ const DataDisplay = ({ data }) => {
         <h1>No games selected</h1>
       ) : (
         <div>
-          <h1>Chess master</h1>
-          <h2>Number of selected games: {data.length}</h2>
-          <h2>Rating gain: {ratingDifference}</h2>
-
           <div className={styles.cards}>
             {ecoForCards.map((item) => (
               <Card key={item} data={gamesPerEco} name={item} />
